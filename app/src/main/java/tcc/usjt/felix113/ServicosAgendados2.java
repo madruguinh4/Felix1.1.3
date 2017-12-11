cc964fc6a920ef480951d1e538e6cd7c43cd8a19
madruguinh4 <alan02david@hotmail.com>
2017-12-11 20:00:11 -0200

    evento do botao cancelar tela servicos agendados2

--- app/src/main/java/tcc/usjt/felix113/ServicosAgendados2.java
+++ app/src/main/java/tcc/usjt/felix113/ServicosAgendados2.java
@@ -3,8 +3,12 @@ package tcc.usjt.felix113;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import tcc.usjt.felix113.View.ViewCliente.TelaCliente;
 
 public class ServicosAgendados2 extends AppCompatActivity {
 
@@ -18,7 +22,7 @@ public class ServicosAgendados2 extends AppCompatActivity {
         String profi = (String) intent.getSerializableExtra("profissao");
         String descri = (String) intent.getSerializableExtra("descricao");
         ImageView imagem = (ImageView) intent.getSerializableExtra("imagem");
-
+        Button btn = (Button)findViewById(R.id.button);
 
         TextView profissao = (TextView)findViewById(R.id.txtServicosAgendados2Profissao);
         TextView descricao = (TextView)findViewById(R.id.txtServicosAgendados2Descricao);
@@ -28,6 +32,14 @@ public class ServicosAgendados2 extends AppCompatActivity {
 
         profissao.setText(profi);
         descricao.setText(descri);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ServicosAgendados2.this,TelaCliente.class);
+                startActivity(intent);
+            }
+        });
 
 
 
